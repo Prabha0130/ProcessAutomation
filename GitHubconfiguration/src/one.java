cbf64d38cf6d46c9496559d21e309abdb908a491
user <praba0130@gmail.com>
2019-06-22 00:28:04 +0530

    vk

--- GitHubconfiguration/src/one.java
+++ GitHubconfiguration/src/one.java
@@ -3,7 +3,7 @@ public class one {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git hub - class 1");
+		System.out.println("Git hub - class 1 2w");
 
 	}
 
